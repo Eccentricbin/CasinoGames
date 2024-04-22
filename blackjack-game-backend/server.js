75731aceedebc72d7a49c4d6d6ba1cd15c24fd9f
rm704 <rm704@students.waikato.ac.nz>
2024-04-22 21:25:03 +1200

    server config

--- blackjack-game-backend/server.js
+++ blackjack-game-backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/blackjack', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://127.0.0.1:27017/blackjack', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
